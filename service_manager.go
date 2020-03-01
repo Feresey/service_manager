90a888cfed687f02ba364e2370119edefbf7ae11
Darkclainer <Darkclainer@gmail.com>
2020-03-01 23:26:20 +0300

    Adapt manager to new API

--- service_manager.go
+++ service_manager.go
@@ -131,7 +131,7 @@ loop:
 	sm.wgMerge.Wait()
 }
 
-var schedulesFunc = map[TaskType]func(root string, states map[string]State, requirements map[string][]string) []string{
+var scheduleFuncMap = map[TaskType]func(root string, states map[string]State, requirements map[string][]string) []string{
 	TaskExit: func(root string, states map[string]State, requirements map[string][]string) []string {
 		return GetEnabledLeafs(states, requirements)
 	},
@@ -140,73 +140,33 @@ var schedulesFunc = map[TaskType]func(root string, states map[string]State, requ
 }
 
 func (sm *ServiceManager) applyTask(task TaskMessage, changed map[string]struct{}) bool {
-	switch task.Task {
-	case TaskStart:
-		if isStartedState(sm.states[task.Name]) == true {
-			return tasks[1:]
-		}
-		if sm.startService(task.Name) == true {
-			return tasks[1:]
-		}
-	case TaskStop:
-		if !isStartedState(sm.states[task.Name]) == true {
-			return tasks[1:]
-		}
-		if sm.stopService(task.Name) == true {
-			return tasks[1:]
-		}
-	case TaskExit:
-	}
-
-	return tasks
-}
-
-func (sm *ServiceManager) stopAllServices() {
-	serviceToStop := GetOrphanedStartedServices(sm.states, sm.requirements)
-	if len(serviceToStop) == 0 {
-		return
-	}
-	toStop := make(map[string]struct{})
-	for _, name := range serviceToStop {
-		toStop[name] = struct{}{}
-		sm.stopService(name)
-	}
-loop:
-	for {
-		select {
-		case <-sm.taskChannel:
-			continue loop
-		case state := <-sm.stateChannel:
-			sm.states[state.Name] = state.State
-			if !isStartedState(state.State) {
-				delete(toStop, state.Name)
-			}
-
-		}
-		for name, _ := range toStop {
-			sm.stopService(name)
-		}
-		if len(toStop) == 0 {
-			return
+	scheduleFunc := scheduleFuncMap[task.Task]
+	schedule := scheduleFunc(task.Name, sm.states, sm.requirements)
+	// filter schedule to get what we should activate
+	n := 0
+	for _, x := range schedule {
+		if _, ok := changed[x]; ok == false {
+			schedule[n] = x
+			n++
 		}
 	}
-
-}
-
-// A: B C
-// C: D
-// B: D E
-// E: F
-//
-func (sm *ServiceManager) startService(name string) bool {
-	for _, requirement := range sm.requirements[name] {
-		if sm.startService(requirement) == false {
-			return false
-		}
-	}
-	if sm.states[name] == StateRunning {
+	schedule = schedule[:n]
+	if len(schedule) == 0 {
 		return true
 	}
+
+	taskFunc := sm.stopService
+	if task.Task == TaskStart {
+		taskFunc = sm.startService
+	}
+
+	for _, name := range schedule {
+		taskFunc(name)
+	}
+	return false
+}
+
+func (sm *ServiceManager) startService(name string) {
 	if !isStartedState(sm.states[name]) {
 		serviceChan := sm.services[name].Start(nil)
 		sm.states[name] = StateStarted
@@ -218,20 +178,10 @@ func (sm *ServiceManager) startService(name string) bool {
 			sm.wgMerge.Done()
 		}()
 	}
-	return false
 }
 
-func (sm *ServiceManager) stopService(name string) bool {
-	for _, requirement := range sm.requirements[name] {
-		if sm.stopService(requirement) == false {
-			return false
-		}
-	}
-	if !isStartedState(sm.states[name]) {
-		return true
-	}
+func (sm *ServiceManager) stopService(name string) {
 	if isStartedState(sm.states[name]) {
 		sm.services[name].Stop()
 	}
-	return false
 }
